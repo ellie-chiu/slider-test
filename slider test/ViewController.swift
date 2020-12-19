eac435be8655482cc6a5646976d0b7267bf294f6
ellie-chiu <chius971037@gmail.com>
2020-12-19 10:39:50 +0800

    Initial Commit

---
+++ slider test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  slider test
+//
+//  Created by TINA on 2020/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
